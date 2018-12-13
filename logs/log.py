b9ed8377779e1fe33c9a07c075383c70dc59bcab
Miroslav Batchkarov <miroslav@skim.it>
2018-12-13 16:30:15 +0000

    format all logging consistently

--- logs/log.py
+++ logs/log.py
@@ -7,11 +7,10 @@ from logging import StreamHandler
 def setup_logging():
     """Set up logging file handler for both app and sqs consumer"""
     file_handler = TimedRotatingFileHandler("logs/autoscaling.log", "D", 1, 10)
-    file_handler.setFormatter(
-        logging.Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
-    )
+    format_ = "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+    file_handler.setFormatter(logging.Formatter(format_))
     stream_handler = StreamHandler(sys.stdout)
-    stream_handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+    stream_handler.setFormatter(logging.Formatter(format_))
     logger_instance = logging.getLogger("autoscaling")
     logger_instance.addHandler(file_handler)
     logger_instance.addHandler(stream_handler)
